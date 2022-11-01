97fa84032e0724ae3377c2cc02079382de2f012e
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-01 08:52:21 -0700

    Make and pass iteration 2

--- lib/race.rb
+++ lib/race.rb
@@ -2,8 +2,17 @@ require 'pry'
 require './lib/candidate'
 
 class Race
+  attr_reader :office, :candidates
 
-  def initialize(role)
+  def initialize(office)
+    @office = office
+    @candidates = []
 
+  end
+
+  def register_candidate!(info)
+    new_candidate = Candidate.new(info)
+    @candidates.push(new_candidate)
+    new_candidate
   end
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe 'Race class' do
 
     expect(race).to be_a(Race)
   end
+
+  it 'has an office role' do
+    race = Race.new("Texas Governor")
+
+    expect(race.office).to eq("Texas Governor")
+  end
+
+  describe '#register_candidate!' do
+    it 'will register a candidate in the Candidate class and add them to the coordinates attribute' do
+      race = Race.new("Texas Governor")
+
+      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+
+      expect(candidate1).to be_a(Candidate)
+      expect(candidate2).to be_a(Candidate)
+      expect(race.candidates).to eq([candidate1, candidate2])
+    end
+  end
 end
