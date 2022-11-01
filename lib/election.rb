43f7e569e8a2f7c0b10ace914f5ec23e67f3ba89
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-11-01 09:53:04 -0700

    Pass all tests in iteraltion 3

--- lib/election.rb
+++ lib/election.rb
@@ -10,4 +10,26 @@ class Election
     @races = []
 
   end
+
+  def add_race(race)
+    @races << race
+  end
+
+  def candidates_array
+    @races.map do |race|
+      race.candidates
+    end
+  end
+
+  def candidates
+    candidates_array.flatten
+  end
+
+  def vote_counts
+    votes = {}
+    candidates.each do |candidate|
+      votes[candidate.name] = candidate.votes
+    end
+    votes
+  end
 end
--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -21,4 +21,65 @@ RSpec.describe 'Election class' do
 
     expect(election.races).to eq([])
   end
+
+  describe '#add_race(race)' do
+    it 'will add a race to its races array' do
+    election = Election.new('2022')
+
+    race1 = Race.new("Virginia District 4 Representative")
+    race2 = Race.new("Texas Governor")
+
+    election.add_race(race1)
+    election.add_race(race2)
+
+    expect(election.races).to eq([race1, race2])
+    end
+  end
+
+  describe '#candidates' do
+
+    it 'will list a single array of all candidates in the races' do
+      election = Election.new('2022')
+
+      race1 = Race.new("Virginia District 4 Representative")
+      race2 = Race.new("Texas Governor")
+
+      election.add_race(race1)
+      election.add_race(race2)
+
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+      candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+      candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+
+      expect(election.candidates).to eq([candidate1, candidate2, candidate3, candidate4, candidate5])
+    end
+  end
+    
+  describe '#vote_counts' do
+    it 'will list all total votes for all candidates' do
+      election = Election.new('2022')
+
+      race1 = Race.new("Virginia District 4 Representative")
+      race2 = Race.new("Texas Governor")
+
+      election.add_race(race1)
+      election.add_race(race2)
+
+      candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+      candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+      candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+      candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+
+      4.times {candidate1.vote_for!}
+      1.times {candidate2.vote_for!}
+      10.times {candidate3.vote_for!}
+      6.times {candidate4.vote_for!}
+      6.times {candidate5.vote_for!}
+
+      expect(election.vote_counts).to eq({"Diana D"=>4, "Roberto R"=>1, "Diego D"=>10, "Rita R"=>6, "Ida I"=>6})
+    end
+  end
 end
